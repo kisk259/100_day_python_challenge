b06c8e21b71292bf11ff54e91feb42be77569ae8
kisk259 <35398085+kisk259@users.noreply.github.com>
2022-02-05 00:35:54 +0100

    first day

---
+++ 1_day/final_of_day_1/main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Welcome stranger! Wanna generate some cool names? This is where you can do this!")
+#2. Ask the user for the city that they grew up in.
+city = input("Name the city where you grew up!\n")
+#3. Ask the user for the name of a pet.
+pet = input("Name of your pet!\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Here you go! Your cool new band name is:" + city + " " + pet)
