92fa8ac3a847a25fbee0b7aa8ff70c2f324d927f
kisk259 <35398085+kisk259@users.noreply.github.com>
2022-04-24 14:10:07 +0200

    first day

---
+++ 2_day/data_types/main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+string_num = str(two_digit_number)
+
+a = int(string_num[0])
+b = int(string_num[1])
+
+print(a+b)
+
+
+
+
---
+++ 2_day/math_operators/main.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+num_height = float(height)
+num_weight = float(weight)
+
+result = num_weight / (num_height ** 2)
+print(int(result))
+
+
+
+
+
+
+
+
